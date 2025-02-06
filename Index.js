3f0e3cee1b633f42496f06269f9f9eb8813bf548
anshun-raj <anshur61500@gmail.com>
2025-02-06 20:32:06 +0530

    first commit

---
+++ Index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $(this).text($(this).text()=='Know More'? 'Know Less':'Know More')
+    })
+})
+
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $(this).text($(this).text()=='Connect'? 'Connecting...':'Connect')
+    })
+})
+
+$(document).ready(function(){
+    $(".btn2").click(function(){
+        $(this).text($(this).text()=='Add to Cart'? 'Added':'Add to Cart')
+    })
+})
+
+
+
+
+
